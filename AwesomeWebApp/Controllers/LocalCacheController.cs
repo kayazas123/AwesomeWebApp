4d5a444071692daa646ac88e02814a0350897755
Yochay Kiriaty <yochay@gmail.com>
2016-01-27 19:19:27 -0800

    added local cache demo

---
+++ AwesomeWebApp/Controllers/LocalCacheController.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+
+namespace AwesomeWebApp.Controllers
+{
+    public class LocalCacheController : Controller
+    {
+        // GET: CdnDemo
+        public ActionResult Index()
+        {
+            ViewBag.LocalCacheReady = true;
+
+            string value = null;
+            if((value=System.Environment.GetEnvironmentVariable("WEBSITE_LOCALCACHE_READY")) == null)
+            { ViewBag.LocalCacheReady = false; }
+
+            return View();
+        }
+    }
+}
